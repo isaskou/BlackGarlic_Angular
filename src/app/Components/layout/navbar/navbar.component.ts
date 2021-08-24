616a3422606a6505e2d2786d6515481c4c1d24ec
isaskou <isabel.skou@yahoo.fr>
2021-08-24 10:39:38 +0200

    add layout + Home Components - TODO : add footer and update home page

---
+++ src/app/Components/layout/navbar/navbar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  navbarItem: Array<any>=[
+    {name:'Home', link:'/home'},
+    {name:'About', link:'/about'},
+    {name:'Shop', link:'/shop'},
+    {name:'Blog', link:'/blog'},
+    {name:'Contact', link:'/contact'}
+
+  ]
+
+  collapsed = true;
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
