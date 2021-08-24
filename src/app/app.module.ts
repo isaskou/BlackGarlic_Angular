4232437c3c8c49955a4ac850b2583a59f1b4bb43
isaskou <isabel.skou@yahoo.fr>
2021-08-24 10:41:41 +0200

    update for Home + layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { HomeBannerComponent } from './Components/Pages/home/home-banner/home-banner.component';
+import { LayoutComponent } from './Components/layout/layout.component';
+import { NavbarComponent } from './Components/layout/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeBannerComponent,
+    LayoutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
