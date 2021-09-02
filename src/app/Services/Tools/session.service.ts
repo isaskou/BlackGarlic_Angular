177355ddc04f4180ee4ba60f33ba3205c10d69f3
isaskou <isabel.skou@yahoo.fr>
2021-09-02 12:40:48 +0200

    update session service --to be continued

--- src/app/Services/Tools/session.service.ts
+++ src/app/Services/Tools/session.service.ts
@@ -4,6 +4,25 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SessionService {
+  decrypted_token: any;
 
   constructor() { }
+
+  recoverCustomerId(): number{
+    if (sessionStorage.getItem("userInfo") != null) {
+      let userId;
+      userId = JSON.parse(sessionStorage.getItem("userInfo"))
+      return userId["userId"];
+    }
+    return null;
+  }
+
+  recoverRoleUser(): string{
+    if (sessionStorage.getItem("userInfo") != null) {
+      let role;
+      role = JSON.parse(sessionStorage.getItem("userInfo"))
+      return role["role"];
+    }
+    return null;
+  }
 }
