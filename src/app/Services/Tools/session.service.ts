09bd28abc1c0fa6e9f3bdd272c97e0c5144a7d3a
isaskou <isabel.skou@yahoo.fr>
2021-09-03 11:15:33 +0200

    continue session Service w/ tempCart

--- src/app/Services/Tools/session.service.ts
+++ src/app/Services/Tools/session.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Cart } from 'src/app/Models/Caddie/cart';
+import { LogInfo } from 'src/app/Models/People/log-info';
+import jwt_decode from "../../../../node_modules/jwt-decode";
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +12,7 @@ export class SessionService {
 
   constructor() { }
 
-  recoverCustomerId(): number{
+  recoverCustomerId(): number {
     if (sessionStorage.getItem("userInfo") != null) {
       let userId;
       userId = JSON.parse(sessionStorage.getItem("userInfo"))
@@ -17,7 +21,7 @@ export class SessionService {
     return null;
   }
 
-  recoverRoleUser(): string{
+  recoverRoleUser(): string {
     if (sessionStorage.getItem("userInfo") != null) {
       let role;
       role = JSON.parse(sessionStorage.getItem("userInfo"))
@@ -25,4 +29,70 @@ export class SessionService {
     }
     return null;
   }
+
+  private _connectedUser: LogInfo;
+
+  get connectedUser(): LogInfo {
+    return this._connectedUser;
+  }
+
+  set connectedUser(value: LogInfo) {
+    this._connectedUser = value;
+  }
+
+  UpdateSessionInformation(response: any) {
+    const token = (<any>response).token;
+    sessionStorage.setItem("jwt", token);
+    this.decrypted_token = jwt_decode(token);
+    let dt = new LogInfo(
+      this.decrypted_token["UserId"],
+      this.decrypted_token["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"],
+      this.decrypted_token["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"],
+      this.decrypted_token["exp"]
+    );
+    sessionStorage.setItem("userInfo", JSON.stringify(dt));
+  }
+
+  StoreTempCart(product: Cart): void{
+    let tmpCart: Cart[] = JSON.parse(sessionStorage.getItem("tmpCart"));
+    if (tmpCart == null || tmpCart == undefined) {
+      tmpCart = [];
+    }
+    tmpCart.push(product);
+    sessionStorage.setItem("tmpcart", JSON.stringify(tmpCart));
+  }
+
+  emptyCart(): void {        
+    sessionStorage.removeItem("tmpCart");
+    sessionStorage.removeItem("cart");
+  }
+  
+  removeTempCartData(product: Cart) : void{    
+    let tmpCart : Cart[] = JSON.parse(sessionStorage.getItem("tmpCart"));
+    tmpCart.splice(tmpCart.findIndex(x => x.id == product.id));
+    sessionStorage.setItem("tmpCart", JSON.stringify(tmpCart));
+  }
+
+  getTempCart() : Cart[] {
+    return JSON.parse(sessionStorage.getItem("tmpCart"));
+  }
+
+  getCartQt() : number {
+    let jsonQt = sessionStorage.getItem("tmpCart");
+    if(jsonQt == undefined || jsonQt == null) {
+      return 0;
+    }
+    else {
+      let qt = JSON.parse(sessionStorage.getItem("tmpCart"))?.length;
+      if(qt != undefined || qt != null){
+        return qt
+      }
+      return 0;
+    }
+  }
+
+
+
+
 }
+
