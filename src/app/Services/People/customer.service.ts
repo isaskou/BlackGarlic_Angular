05d29905cf01b0b16aef83eea7a3abae9956e97f
isaskou <isabel.skou@yahoo.fr>
2021-09-03 11:21:57 +0200

    update customerService w/ GetCurrent+UpdateCustomer

--- src/app/Services/People/customer.service.ts
+++ src/app/Services/People/customer.service.ts
@@ -5,6 +5,7 @@ import { ICustomer } from 'src/app/Interfaces/People/icustomer';
 import { Customer } from 'src/app/Models/People/customer';
 import { RegisterCustomer } from 'src/app/Models/People/register-customer';
 import { environment } from 'src/environments/environment';
+import { SessionService } from '../Tools/session.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,22 +15,20 @@ export class CustomerService {
   private url = environment.apiUrl + 'Customer/api/Customer/';
 
   constructor(
-    private _http: HttpClient
-    //TODO
-    // private sessionService : SessionStorageService
+    private _http: HttpClient,
+    private _sessionService : SessionService
   ) { }
 
   getCustomerById(id: number): Subscribable<ICustomer>{
     return this._http.get(this.url + id);
   }
 
-  //TODO
-  // getCurrentCustomer(): Subscribable<ICustomer>{
-  //  
-  // }
-  // UpdateCustomerInformation(customer: Customer): Subscribable<any>{
-    
-  // }
+  getCurrentCustomer(): Subscribable<ICustomer>{
+    return this._http.get(this.url + this._sessionService.recoverCustomerId());
+  }
+  UpdateCustomerInformation(customer: Customer): Subscribable<any>{
+    return this._http.put(this.url + this._sessionService.recoverCustomerId(), customer);
+  }
 
   RegisterCustomer(customer: RegisterCustomer): Subscribable<RegisterCustomer>{
     return this._http.post(this.url, customer);
