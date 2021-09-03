962de46523f5a254af21f5b06daaa1b0b1b24b6e
isaskou <isabel.skou@yahoo.fr>
2021-09-03 11:17:57 +0200

    add Token Interceptor

--- src/app/Services/Tools/token.service.ts
+++ src/app/Services/Tools/token.service.ts
@@ -1,9 +1,21 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenService {
+export class TokenService implements HttpInterceptor{
 
   constructor() { }
+
+  intercept(req: HttpRequest<any>,
+    next: HttpHandler): Observable<HttpEvent<any>> {
+      let tokenizedReq = req.clone({
+        setHeaders: {
+          Authorization: 'bearer '+ sessionStorage.getItem("jwt")
+        }
+      })
+      return next.handle(tokenizedReq);
+    }  
 }
