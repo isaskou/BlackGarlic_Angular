7dcd836311e513ce653684265b0043a94dd76e31
isaskou <isabel.skou@yahoo.fr>
2021-09-03 14:21:38 +0200

    add logCustomer in loginService

--- src/app/Services/People/login.service.ts
+++ src/app/Services/People/login.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subscribable, SubscribableOrPromise } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private url = environment.apiUrl + "Login/";
+
+  constructor(
+    private _http:HttpClient
+  ) { }
+
+  logCustomer(credentials: any): Subscribable<any>{
+    return this._http.post<any>(this.url, credentials);
+  }
 }
