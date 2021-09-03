90b84ed8e0ade34b5314bbf02879ab8d57be3891
isaskou <isabel.skou@yahoo.fr>
2021-09-03 16:00:31 +0200

    comment html for forms + add var/dependanceInjectin -- to be continued

--- src/app/Components/Pages/account/account.component.ts
+++ src/app/Components/Pages/account/account.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomerService } from 'src/app/Services/People/customer.service';
+import { LoginService } from 'src/app/Services/People/login.service';
+import { SessionService } from 'src/app/Services/Tools/session.service';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  registerForm: FormGroup;
+  decrypted_token: any;
+  emailExist: boolean;
+  noUserFound: boolean;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+    private _customerService: CustomerService,
+    private _sessionService: SessionService,
+    private _loginService:LoginService
+  ) { }
 
   ngOnInit(): void {
+    if (this._sessionService.recoverCustomerId() != null) {
+      this._router.navigateByUrl("/shop");
+    }
+
+    
   }
 
 }
