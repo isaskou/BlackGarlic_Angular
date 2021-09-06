df44551b067a76e21762a9c8cba4ebed66380985
isaskou <isabel.skou@yahoo.fr>
2021-09-06 17:56:35 +0200

    update caddie component w/ AddToTempCart

--- src/app/Components/Caddie/panier/panier.component.ts
+++ src/app/Components/Caddie/panier/panier.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Cart } from 'src/app/Models/Caddie/cart';
+import { Command } from 'src/app/Models/Caddie/command';
+import { CommandProduct } from 'src/app/Models/Caddie/command-product';
 import { CommandService } from 'src/app/Services/Caddie/command.service';
+import { SessionService } from 'src/app/Services/Tools/session.service';
 
 @Component({
   selector: 'app-panier',
@@ -16,14 +19,34 @@ export class PanierComponent implements OnInit {
 
   constructor(
     private _router: Router,
-    //TODO
-    // private _sessionService: SessionStorageService,
+    private _sessionService: SessionService,
     private _commandService: CommandService,
   ) { }
 
   ngOnInit(): void {
+    this.cart = this._sessionService.getTempCart();
     for (let i = 0; i < this.cart.length; i++){
       this.total = parseInt(this.total.toString()) + (parseInt(this.cart[i].unitPrice.toString()) * this.cart[i].quantity);
+    }
+  }
+
+  payCart(): void{
+    if (this.adressesSelected) {
+      if (this._sessionService.recoverCustomerId() == undefined) {
+        this._router.navigateByUrl("main/login");
+      }
+      else {
+        let cmdProduct: CommandProduct[] = [];
+        for (let i = 0; i < this.cart.length; i++){
+          cmdProduct.push(new CommandProduct(this.cart[i].quantity, this.cart[i].id));
+        }
+        let cmd: Command;
+        cmd = new Command(0, this._sessionService.recoverCustomerId(), this.adressSelectedId, cmdProduct);
+        this._commandService.sendCommandToApi(cmd).subscribe(dt => {
+          this._sessionService.emptyCart();
+          this._router.navigateByUrl("/detailCommand/" + dt);
+        });
+      }
     }
   }
 
@@ -35,7 +58,12 @@ export class PanierComponent implements OnInit {
   }
 
   removeFromCart(productId: number) {
-    // TODO
+    let tmpCart: Cart[] = this._sessionService.getTempCart();
+    let cartToRemove: Cart = tmpCart.find(x => x.id == productId);
+    tmpCart.splice(tmpCart.findIndex(x => x.id == productId), 1);
+    sessionStorage.setItem("tmpCart", JSON.stringify(tmpCart));
+    this.cart.splice(tmpCart.findIndex(x => x.id == productId));
+    this.total = parseInt(this.total.toString()) - (cartToRemove.unitPrice * cartToRemove.quantity);
     
   }
 
